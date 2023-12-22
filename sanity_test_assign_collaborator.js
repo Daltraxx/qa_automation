9dcf9b9bad1a111d433b86d7f489e100dab9d71a
Dalton Pettus <dalton@robotcache.com>
2023-12-22 10:01:32 -0800

    modify to not assign people on weekends

--- sanity_test_assign_collaborator.js
+++ sanity_test_assign_collaborator.js
@@ -1,7 +1,7 @@
 //get correct table and query records with necessary fields
 let table = base.getTable("QA Testing");
 let query = await table.selectRecordsAsync({
-    fields: ["Daily Sanity", "Tester"]
+    fields: ["Daily Sanity", "Sanity Tester"]
 });
 
 let recordDetails = query.records;
@@ -9,7 +9,7 @@ let recordDetails = query.records;
 //get and assign values for fields in query
 recordDetails.forEach((record) => {
     record["Daily Sanity"] = record.getCellValueAsString("Daily Sanity");
-    record["Tester"] = record.getCellValueAsString("Tester");
+    record["Sanity Tester"] = record.getCellValueAsString("Sanity Tester");
 });
 
 //create empty array and then store record ids which have Daily Sanity checked, i.e. the ones that need a Tester
@@ -36,27 +36,35 @@ allCollaborators.forEach((collaborator) => {
 
 //get today's date
 const date = new Date();
-let day = date.getDate();
+const day = date.getDate();
+const dayName = date.getDay();
+
 
 //create arrays of dates for which people will be responsible for sanity check
-let lawrenceDates = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31];
-let charlesDates = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29];
-let dougDates = [3, 6 ,9, 12, 15, 18, 21, 24, 27, 30];
+const lawrenceDates = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31];
+const charlesDates = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29];
+const dougDates = [3, 6 ,9, 12, 15, 18, 21, 24, 27, 30];
 
-//get "today's tester" based on date
+//get "today's tester" based on date, but not on weekends
 let todaysTester;
-if (lawrenceDates.includes(day)) {
-    todaysTester = qaCollaborators[1];
-} else if (charlesDates.includes(day)) {
-    todaysTester = qaCollaborators[2];
-} else {
-    todaysTester = qaCollaborators[0];
+if (dayName !== 6 && dayName !== 0) {
+    if (lawrenceDates.includes(day)) {
+        todaysTester = qaCollaborators[1];
+    } else if (charlesDates.includes(day)) {
+        todaysTester = qaCollaborators[2];
+    } else {
+        todaysTester = qaCollaborators[0];
+    }
 };
+
 
 
 //update records with assigned tester
-for (let i = 0; i < recordsToBeUpdated.length; i++) {
-    await table.updateRecordAsync(recordsToBeUpdated[i], {
-    "Tester": todaysTester,
-    })
+if (todaysTester) {
+    for (let i = 0; i < recordsToBeUpdated.length; i++) {
+        await table.updateRecordAsync(recordsToBeUpdated[i], {
+            "Sanity Tester": todaysTester,
+        })
+    }
 };
+
